fdf89fc9635a24795c17bd0d097680325ee98617
Hackdarkyeh <105940561+Hackdarkyeh@users.noreply.github.com>
2023-04-26 12:34:22 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,11 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log('Formulario enviado'); // Agrega esta línea para ver si se ejecuta este evento
-  const username = form.elements.usuario.value;
-  const password = form.elements.contraseña.value;
-  console.log(username, password); // Agrega esta línea para ver los valores de usuario y contraseña
-  if (username === 'usuario' && password === 'tu_contraseña') {
-    console.log('Ingreso correcto'); // Agrega esta línea para ver si se ejecuta este evento
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+  if (username === 'tu_usuario' && password === 'tu_contraseña') {
     window.location.href = 'https://github.com/Hackdarkyeh/paginadiscord/blob/main/inicio.html';
   } else {
-    console.log('Ingreso incorrecto'); // Agrega esta línea para ver si se ejecuta este evento
     alert('Usuario o contraseña incorrectos');
   }
 });
