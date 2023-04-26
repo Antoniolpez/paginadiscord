b82026074c73365622719e15cba8ab9a30518440
Hackdarkyeh <105940561+Hackdarkyeh@users.noreply.github.com>
2023-04-26 12:53:36 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,14 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const username = form.elements.usuario.value;
-  const password = form.elements.contraseña.value;
-  if (username === 'tu_usuario' && password === 'tu_contraseña') {
-    window.location.href = 'https://github.com/Hackdarkyeh/paginadiscord/blob/main/inicio.html';
-  } else {
-    alert('Usuario o contraseña incorrectos');
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = form.elements.usuario.value;
+    const password = form.elements.contraseña.value;
+    if (username === 'tu_usuario' && password === 'tu_contraseña') {
+      window.location.href = 'https://github.com/Hackdarkyeh/paginadiscord/blob/main/inicio.html';
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  });
 });
+
