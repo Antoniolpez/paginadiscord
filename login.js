c77a8dfe41e7e250857fc09a046c7d6f542a3a45
Hackdarkyeh <105940561+Hackdarkyeh@users.noreply.github.com>
2023-05-01 12:36:14 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,54 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
   let intentos = 0;
-    let tiempo = 0;
-    let intervalo = null;
-    const mensaje = document.querySelector('#mensaje');
-    const contador = document.createElement('span');
-    contador.innerText = '00:00';
-    mensaje.appendChild(contador);
-    
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const username = form.elements.usuario.value;
-      const password = form.elements.password.value;
-      if (username === 'tu_usuario' && password === 'tu_contraseña') {
-        window.location.href = 'https://hackdarkyeh.github.io/paginadiscord/inicio.html';
-      } else {
-        intentos++;
-        if (intentos >= 3) {
-          clearInterval(intervalo);
-          form.elements.usuario.disabled = true;
-          form.elements.password.disabled = true;
-          const tiempoRestante = contador.innerText;
-          mensaje.innerText = `Demasiados intentos fallidos. Por favor, espere ${tiempoRestante} antes de volver a intentarlo.`;
-          return;
-        }
-        alert('Usuario o contraseña incorrectos, intentelo de nuevo');
-      }
-    });
+  let tiempo = 0;
+  let intervalo = null;
+  const mensaje = document.querySelector('#mensaje');
+  const contador = document.createElement('span');
+  contador.innerText = '00:00';
+  mensaje.appendChild(contador);
+  const tiempoRestante = document.querySelector('#tiempo-restante');
 
-    const contarTiempo = () => {
-      tiempo--;
-      if (tiempo <= 0) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = form.elements.usuario.value;
+    const password = form.elements.password.value;
+    if (username === 'tu_usuario' && password === 'tu_contraseña') {
+      window.location.href = 'https://hackdarkyeh.github.io/paginadiscord/inicio.html';
+    } else {
+      intentos++;
+      if (intentos >= 3) {
         clearInterval(intervalo);
-        form.elements.usuario.disabled = false;
-        form.elements.password.disabled = false;
-        contador.innerText = '';
-        mensaje.innerText = '';
-      } else {
-        const minutos = Math.floor(tiempo / 60).toString().padStart(2, '0');
-        const segundos = (tiempo % 60).toString().padStart(2, '0');
-        contador.innerText = `${minutos}:${segundos}`;
+        form.elements.usuario.disabled = true;
+        form.elements.password.disabled = true;
+        tiempoRestante.style.display = 'block';
+        const tiempoRestanteTexto = contador.innerText;
+        mensaje.innerText = `Demasiados intentos fallidos. Por favor, espere ${tiempoRestanteTexto} antes de volver a intentarlo.`;
+        return;
       }
+      alert('Usuario o contraseña incorrectos, intentelo de nuevo');
     }
+  });
 
-    const iniciarContador = () => {
-      tiempo = 300;
-      intervalo = setInterval(contarTiempo, 1000);
+  const contarTiempo = () => {
+    tiempo--;
+    if (tiempo <= 0) {
+      clearInterval(intervalo);
+      form.elements.usuario.disabled = false;
+      form.elements.password.disabled = false;
+      contador.innerText = '';
+      mensaje.innerText = '';
+      tiempoRestante.style.display = 'none';
+    } else {
+      const minutos = Math.floor(tiempo / 60).toString().padStart(2, '0');
+      const segundos = (tiempo % 60).toString().padStart(2, '0');
+      contador.innerText = `${minutos}:${segundos}`;
     }
+  };
 
-    iniciarContador();
+  const iniciarContador = () => {
+    tiempo = 300;
+    intervalo = setInterval(contarTiempo, 1000);
+  };
+
+  iniciarContador();
 });
- 
-
