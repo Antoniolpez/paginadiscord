30e3518d562e6df318b5aeb3915b2e1cd6e77143
Hackdarkyeh <105940561+Hackdarkyeh@users.noreply.github.com>
2023-05-01 13:07:43 +0200

    Update login.js

--- login.js
+++ login.js
@@ -9,6 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
   mensaje.appendChild(contador);
   const tiempoRestante = document.querySelector('#tiempo-restante');
 
+  // Verifica si el usuario está bloqueado
+  if (localStorage.getItem('bloqueado') === 'true') {
+    form.elements.usuario.disabled = true;
+    form.elements.password.disabled = true;
+    tiempoRestante.style.display = 'block';
+    const tiempoRestanteTexto = localStorage.getItem('tiempoRestante');
+    contador.innerText = tiempoRestanteTexto;
+    mensaje.innerText = `Demasiados intentos fallidos. Por favor, espere ${tiempoRestanteTexto} antes de volver a intentarlo.`;
+    tiempo = parseInt(localStorage.getItem('tiempo'));
+    iniciarContador();
+  }
+
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const username = form.elements.usuario.value;
@@ -24,6 +36,10 @@ document.addEventListener('DOMContentLoaded', () => {
         tiempoRestante.style.display = 'block';
         const tiempoRestanteTexto = contador.innerText;
         mensaje.innerText = `Demasiados intentos fallidos. Por favor, espere ${tiempoRestanteTexto} antes de volver a intentarlo.`;
+        // Guarda el estado del bloqueo en localStorage
+        localStorage.setItem('bloqueado', 'true');
+        localStorage.setItem('tiempoRestante', tiempoRestanteTexto);
+        localStorage.setItem('tiempo', tiempo);
         return;
       }
       alert('Usuario o contraseña incorrectos, intentelo de nuevo');
@@ -39,6 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
       contador.innerText = '';
       mensaje.innerText = '';
       tiempoRestante.style.display = 'none';
+      // Borra el estado del bloqueo en localStorage
+      localStorage.removeItem('bloqueado');
+      localStorage.removeItem('tiempoRestante');
+      localStorage.removeItem('tiempo');
     } else {
       const minutos = Math.floor(tiempo / 60).toString().padStart(2, '0');
       const segundos = (tiempo % 60).toString().padStart(2, '0');
@@ -47,9 +67,12 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   const iniciarContador = () => {
-    tiempo = 300;
     intervalo = setInterval(contarTiempo, 1000);
   };
 
-  iniciarContador();
+  // Inicia el contador solo si el usuario no está bloqueado
+  if (!localStorage.getItem('bloqueado')) {
+    tiempo = 300;
+    iniciarContador();
+  }
 });
