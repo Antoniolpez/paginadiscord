287b575ae87d36767d3e79a164e0128fce6461ec
Hackdarkyeh <105940561+Hackdarkyeh@users.noreply.github.com>
2023-05-13 18:40:30 +0200

    Update login.js

--- login.js
+++ login.js
@@ -97,14 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
 
-function toggleTheme() {
-	var theme = document.getElementById("theme");
-	if (theme.getAttribute("href") == "light.css") {
-		theme.setAttribute("href", "dark.css");
-	} else {
-		theme.setAttribute("href", "light.css");
-	}
-}
+
 
 
 
