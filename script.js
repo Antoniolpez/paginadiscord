563a23920398714d29e50221732e0cd0136ddd0a
Hackdarkyeh <105940561+Hackdarkyeh@users.noreply.github.com>
2023-05-13 19:13:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toggleTheme() {
 	var theme = document.getElementById("theme");
+	console.log("Ejecuta!");
 	if (theme.getAttribute("href") == "light.css") {
+		console.log("Ejecuta1!");
 		theme.setAttribute("href", "dark.css");
 	} else {
 		theme.setAttribute("href", "light.css");
+		console.log("Ejecuta2!");
+
 	}
 }
